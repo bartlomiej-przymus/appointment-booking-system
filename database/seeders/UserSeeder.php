5d85580816d207bbd46fcef09072e49f3761edeb
Bartlomiej Przymus <43916409+bartlomiej-przymus@users.noreply.github.com>
2024-11-09 13:01:17 +0000

    feat: add missing seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,12 +7,10 @@ use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         User::factory()->create([
+            'name' => 'Admin',
             'email' => 'admin@abs.test',
         ]);
     }
