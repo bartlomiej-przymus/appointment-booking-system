a32db297257538afb1c67895d29564dd19f92796
Bartlomiej Przymus <43916409+bartlomiej-przymus@users.noreply.github.com>
2024-11-12 01:46:38 +0000

    test: create test for Schedule model

---
+++ tests/Unit/Model/ScheduleTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Unit\Model;
+
+use App\Models\Schedule;
+
+it('can check if current date is within schedule active date range', function () {
+    $activeSchedule = Schedule::factory()->make([
+        'active_from' => now()->subDay(),
+        'active_to' => now()->addDay(),
+    ]);
+
+    $inactiveSchedule = Schedule::factory()->make([
+        'active_from' => now()->subWeek(),
+        'active_to' => now()->subDay(),
+    ]);
+
+    expect($activeSchedule->isWithinActivePeriod())->toBeTrue()
+        ->and($inactiveSchedule->isWithinActivePeriod())->toBeFalse();
+});
+
+it('can check if active schedules exist', function () {
+    $activeSchedule = Schedule::factory()->active()->create();
+    $newSchedule = Schedule::factory()->create();
+
+    expect($newSchedule->hasActiveSchedules())->toBeTrue();
+
+    $activeSchedule->update(['active' => false]);
+
+    expect($newSchedule->hasActiveSchedules())->toBeFalse();
+});
+
+it('can check if it has valid date range', function () {
+    $schedule = Schedule::factory()->create(['active_from' => null, 'active_to' => null]);
+    expect($schedule->hasValidDateRange())->toBeFalse();
+
+    $schedule->update(['active_from' => now()->subDay(), 'active_to' => now()->addDay()]);
+    expect($schedule->hasValidDateRange())->toBeTrue();
+});
+
+it('can check if schedule is active', function () {
+    $schedule = Schedule::factory()->active()->create([
+        'active_from' => null,
+        'active_to' => null,
+    ]);
+    expect($schedule->isActive())->toBeTrue();
+
+    $schedule->update(['active' => false]);
+
+    expect($schedule->isActive())->toBeFalse();
+});
+
+it('can show schedule as inactive if there is other set to active', function () {
+    $activeSchedule = Schedule::factory()
+        ->active()->create([
+            'active_from' => null,
+            'active_to' => null,
+        ]);
+
+    $schedule = Schedule::factory()->create([
+        'active' => false,
+        'active_from' => now()->subWeek(),
+        'active_to' => now()->addWeek(),
+    ]);
+
+    expect($schedule->isActive())->toBeFalse();
+
+    $activeSchedule->update(['active' => false]);
+
+    expect($schedule->isActive())->toBeTrue();
+});
