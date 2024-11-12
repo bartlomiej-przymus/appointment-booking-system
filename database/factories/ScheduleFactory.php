be204d0434d23d2298687cdcf746b04cd9c9f0b8
Bartlomiej Przymus <43916409+bartlomiej-przymus@users.noreply.github.com>
2024-11-12 01:46:15 +0000

    feat: make schedule factory

--- database/factories/ScheduleFactory.php
+++ database/factories/ScheduleFactory.php
@@ -2,22 +2,31 @@
 
 namespace Database\Factories;
 
+use App\Enums\DayType;
+use App\Enums\ScheduleType;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\schedule>
- */
 class ScheduleFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
+        $activeFrom = fake()->dateTimeBetween('-1 week', '-1 day');
+
         return [
-            //
+            'name' => Str::ucfirst(fake()->word()).' Schedule',
+            'type' => fake()->randomElement(ScheduleType::values()),
+            'excluded_days' => fake()->randomElements(DayType::values(), 2),
+            'active' => fake()->boolean(),
+            'active_from' => $activeFrom,
+            'active_to' => fake()->dateTimeBetween($activeFrom, '+1 month'),
         ];
+    }
+
+    public function active(): Factory
+    {
+        return $this->state(fn (array $attributes) => [
+            'active' => true,
+        ]);
     }
 }
