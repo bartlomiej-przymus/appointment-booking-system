dba76b5d4486365803884965857009449c698fa6
Bartlomiej Przymus <43916409+bartlomiej-przymus@users.noreply.github.com>
2024-11-13 18:23:07 +0000

    fix: activity toggle on schedule update not deactivating other schedules when toggled active

--- app/Filament/Resources/ScheduleResource/Pages/EditSchedule.php
+++ app/Filament/Resources/ScheduleResource/Pages/EditSchedule.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\ScheduleResource\Pages;
 
 use App\Filament\Resources\ScheduleResource;
+use App\Models\Schedule;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class EditSchedule extends EditRecord
 {
@@ -15,5 +17,13 @@ class EditSchedule extends EditRecord
         return [
             DeleteAction::make(),
         ];
+    }
+
+    public function beforeSave(): void
+    {
+        if ($this->data['active']) {
+            Schedule::where('active', true)
+                ->update(['active' => false]);
+        }
     }
 }
