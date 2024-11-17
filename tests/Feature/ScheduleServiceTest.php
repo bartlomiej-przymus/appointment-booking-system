f7ddc6e6be6afe7c37a7e221bade00362f3dea6b
Bartlomiej Przymus <43916409+bartlomiej-przymus@users.noreply.github.com>
2024-11-17 11:51:23 +0000

    test: first test of schedule service class functionality

--- tests/Feature/ScheduleServiceTest.php
+++ tests/Feature/ScheduleServiceTest.php
@@ -6,6 +6,7 @@ use App\Models\Availability;
 use App\Models\Schedule;
 use App\Models\Slot;
 use App\Services\ScheduleService;
+use Carbon\Carbon;
 
 it('can retrieve schedule set to active', function () {
     Schedule::factory()->count(5)->create([
@@ -69,8 +70,6 @@ it('can retrieve schedule that is set to active when another schedule has valid 
 });
 
 it('returns null when no active date range schedule is currently set', function () {
-    $scheduleService = new ScheduleService;
-
     Schedule::factory()->create([
         'active' => false,
         'active_from' => now()->subMonth(),
@@ -83,16 +82,34 @@ it('returns null when no active date range schedule is currently set', function 
         'active_to' => now()->addWeek(),
     ]);
 
-    expect($scheduleService->getActiveSchedule())->toBeNull();
+    expect((new ScheduleService())
+        ->getActiveSchedule())
+        ->toBeNull();
 });
 
-it('returns all available dates for a month if schedule type daily', function () {
-    $scheduleService = new ScheduleService;
+it('returns available dates and time slots for current month when schedule daily', function () {
+    $testDate = Carbon::parse('1st November 2024');
+    Carbon::setTestNow($testDate);
 
-    $availability = Availability::factory()->create([
-        'appointment_duration' => 45,
-        'break' => 15,
-    ]);
+    /**
+     * Current date is set for November 2024
+     * Two Availability Time Slots are created
+     * Type daily Schedule is created set to be
+     * active.
+     *
+     * System has date padding hard-coded
+     * to allow new appointments to be booked
+     * at lest 2 days in advance giving admin
+     * time to prepare.
+     *
+     * Resulting number of days by doing manual
+     * count starting on 1st November 2024 is 27.
+     *
+     * First active day should be 4th as system
+     * adds 2 days of buffer ahead.
+     */
+
+    $availability = Availability::factory()->create();
 
     $slots = Slot::factory()->createMany(
         [
@@ -110,19 +127,19 @@ it('returns all available dates for a month if schedule type daily', function ()
 
     Schedule::factory()->active()->create([
         'type' => ScheduleType::Daily->value,
-        'excluded_days' => [
-            DayType::Saturday->value,
-            DayType::Sunday->value,
-        ],
+        'excluded_days' => [],
         'availability_id' => $availability->getKey(),
     ]);
 
-    $availableDatesForMonth = $scheduleService->getAvailableDatesForMonth(now());
-    dd($availableDatesForMonth->toArray());
-    expect($availableDatesForMonth)
-        ->toBeCollection()
-        ->and($availableDatesForMonth->isEmpty())
+    $availableDatesForMonth = (new ScheduleService())
+        ->getAvailableDatesForMonth(now());
+
+    expect($availableDatesForMonth->isEmpty())
         ->toBeFalse()
-        ->and($availableDatesForMonth->first())
-        ->toContain('11:00', '12:00');
+        ->and($availableDatesForMonth)
+        ->toBeCollection()
+        ->toHaveCount(27)
+        ->each->toContain('11:00', '12:00')
+        ->and($availableDatesForMonth->offsetExists('2024-11-04'))
+        ->toBeTrue();
 });
