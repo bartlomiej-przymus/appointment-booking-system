02398d205521e4c083dae18797a360a47b8a009b
Bartlomiej Przymus <43916409+bartlomiej-przymus@users.noreply.github.com>
2024-11-23 18:37:28 +0000

    feat: update day blade component

--- resources/views/components/calendar/day.blade.php
+++ resources/views/components/calendar/day.blade.php
@@ -4,17 +4,21 @@
     'withinMonth' => true,
     'today' => false,
     'available' => false,
+    'selected' => false,
 ])
 
-<div @class([
+<button {{ $attributes }} @class([
     'font-light', 'h-16', 'flex', 'justify-center', 'items-center', 'aspect-[1/1]',
+    'cursor-default' => ! $available,
     'border' => ! $header,
     'border-accentColor' => $today,
     'border-gray-200' => ! $today,
-    'hover:bg-indigo-200' => $withinMonth && $available,
+    'hover:bg-green-200' => $withinMonth && $available,
     'text-gray-300' => ! $withinMonth,
-    'text-red-600' => $weekend,
-    'bg-indigo-100' => $available,
+    'text-red-600' => $weekend && $withinMonth,
+    'text-red-300' => $weekend && ! $withinMonth,
+    'bg-green-100' => $available,
+    'bg-green-500' => $selected,
 ])>
     {{ $slot }}
-</div>
+</button>
