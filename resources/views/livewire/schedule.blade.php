29314e7fcedde7a37ca2d86bf3a39609a5b84484
Bartlomiej Przymus <43916409+bartlomiej-przymus@users.noreply.github.com>
2025-04-06 21:02:35 +0100

    feat: use getAppointmentDuration method in blade file

--- resources/views/livewire/schedule.blade.php
+++ resources/views/livewire/schedule.blade.php
@@ -19,7 +19,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
                 <div class="text-gray-400">
-                    {{ $schedule->availability->appointment_duration }} min.
+                    {{ $this->getAppointmentDuration() }} min.
                 </div>
             </div>
             <div class="flex flex-row items-center gap-2 text-md text-gray-400">
