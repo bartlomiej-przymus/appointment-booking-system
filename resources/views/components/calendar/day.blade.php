051c360c7865e495d4808a91480496675b158e6f
Bartlomiej Przymus <43916409+bartlomiej-przymus@users.noreply.github.com>
2025-04-17 09:38:10 +0100

    feat: day view update

--- resources/views/components/calendar/day.blade.php
+++ resources/views/components/calendar/day.blade.php
@@ -9,16 +9,19 @@
 
 <button {{ $attributes }} @class([
     'font-light', 'h-14', 'flex', 'justify-center', 'items-center', 'aspect-[1/1]', 'rounded-full',
+    'hover:bg-gray-100' => ! $selected && $withinMonth,
     'cursor-default' => ! $available,
     'border' => ! $header,
     'border-gray-200' => ! $today,
-    'border-accentColor' => $today,
-    'hover:bg-red-200' => $withinMonth && $available,
+    'border-black' => $today,
+    'hover:bg-red-200' => $withinMonth && $available && ! $selected,
     'text-gray-300' => ! $withinMonth,
     'text-red-600' => $weekend && $withinMonth,
     'text-red-300' => $weekend && ! $withinMonth,
     'bg-red-100' => $available,
     'bg-red-500' => $selected,
+    'border-accentColor' => $selected,
+    'text-white' => $selected,
 ])>
     {{ $slot }}
 </button>
