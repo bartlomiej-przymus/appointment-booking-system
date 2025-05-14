7bf6a8e1e99b418c6082761d5ab085efb2215e4d
Bartlomiej Przymus <43916409+bartlomiej-przymus@users.noreply.github.com>
2025-05-14 21:50:39 +0100

    feat: update payment routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,23 @@
 <?php
 
+use App\Http\Controllers\PaymentController;
+use App\Livewire\Payment;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get(
+        '/payment/{appointmentId}',
+        Payment::class
+    )->name('payment');
+    Route::get(
+        '/payment/success',
+        [PaymentController::class, 'success']
+    )->name('payment.success');
+});
+
 
 require __DIR__.'/auth.php';
